058910b493125540570b879daa98facb7ce100c3
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2024-07-24 00:45:06 +0300

    refact: Move PlaySequenceStage class implementation to separate file

--- arduino-color-memory.ino
+++ arduino-color-memory.ino
@@ -4,24 +4,6 @@
 #include "PlaySequenceStage.h"
 #include "StagesLocator.h"
 
-StageInterface* PlaySequenceStage::run() {
-  for (int i = 100; i <= 800; i += 100) {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(i);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(i);                      // wait for a second
-  }
-
-  for (int i = 700; i >= 100; i -= 100) {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(i);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(i);                      // wait for a second
-  }
-
-  return mStagesLocator->startupStage;
-}
-
 void StartupStage::waitForAnyKey() {
   const int buttonPin = 2;
   
