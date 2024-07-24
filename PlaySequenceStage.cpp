92d914a476ce407794cf79090fe58003701cc28d
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2024-07-24 19:44:17 +0300

    refact: Remove unused code

--- PlaySequenceStage.cpp
+++ PlaySequenceStage.cpp
@@ -14,19 +14,6 @@ StageInterface* PlaySequenceStage::run()
         waitFor(pause);
         sequenceDisplay.show(sequence.next(), duration);
     }
-//   for (int i = 100; i <= 800; i += 100) {
-//     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-//     delay(i);                      // wait for a second
-//     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-//     delay(i);                      // wait for a second
-//   }
-
-//   for (int i = 700; i >= 100; i -= 100) {
-//     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-//     delay(i);                      // wait for a second
-//     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-//     delay(i);                      // wait for a second
-//   }
 
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
