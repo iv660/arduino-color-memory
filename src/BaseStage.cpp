5a0ec38b4d674cd2e65a050f7501f9eaf15e041d
Ilya Vikharev <18052192+iv660@users.noreply.github.com>
2024-08-13 01:40:17 +0300

    fix: Move sketch files to the folder named after sketch

--- src/BaseStage.cpp
+++
@@ -1,8 +0,0 @@
-#include "BaseStage.h"
-#include "StagesLocator.h"
-
-BaseStage::BaseStage(
-    StagesLocator* stagesLocator, ServiceLocator* serviceLocator): 
-    serviceLocator(serviceLocator), stagesLocator(stagesLocator) {
-        dashboard = serviceLocator->getDashboard();
-    }
